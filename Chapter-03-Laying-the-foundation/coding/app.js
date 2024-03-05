a6b147d1c0c7465734a94f62468611778c797e36
tanzeem131 <syedtanzeem131@gmail.com>
2024-03-05 16:10:43 +0530

    initial commits

--- Chapter-03-Laying-the-foundation/coding/app.js
+++ Chapter-03-Laying-the-foundation/coding/app.js
@@ -1,14 +1,68 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement(
-  "h1",
-  { id: " heading " },
-  `Let's do some fun with react🤩😉`
+// JSX=>Babel transpiled it to React.createElement => React Element--is a Javascript object => HTMLElement(render)
+
+//React Element
+// const jsxHeading = (
+//   <h1 id="heading" tabIndex="5">
+//     Let's do some fun with react🤩😉
+//   </h1>
+// );
+
+/*
+//React Component
+const Title = () => (
+  <h1 id="heading" tabIndex="5">
+    Let's do some fun with react🤩😉🤩😉🤩😉🤩😉🤩😉
+  </h1>
 );
 
-const jsxHeading = <h1 id="heading">Let's do some fun with react🤩😉</h1>;
+//Component Composition
+const HeadingComponent = () => (
+  <div className="container">
+    <Title />
+    <h1 id="heading">Let's do some fun with react🤩😉</h1>
+  </div>
+);
+
+*/
+
+/* Injecting javascript code in react component using {}
+//React element
+const title = (
+  <h1 id="heading" tabIndex="5">
+    Let's do some fun with react🤩😉🤩😉🤩😉🤩😉🤩😉
+  </h1>
+);
+
+//Component Composition - putting one component into other 
+const HeadingComponent = () => (
+  <div className="container">
+    {title}
+    <h1 id="heading">Let's do some fun with react🤩😉</h1>
+  </div>
+);
+*/
+
+//React component in the end is a javascript function
+const Title = () => (
+  <h1 id="heading" tabIndex="5">
+    Let's do some fun with react🤩😉🤩😉🤩😉🤩😉🤩😉
+  </h1>
+);
+
+//Component Composition
+const HeadingComponent = () => (
+  <div className="container">
+    <Title />
+    <Title></Title>
+    {Title()}
+    <h1 id="heading">Let's do some fun with react🤩😉</h1>
+  </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(jsxHeading);
+// root.render(jsxHeading);
+root.render(<HeadingComponent />);
--- Chapter-03-Laying-the-foundation/readMe.md
+++ Chapter-03-Laying-the-foundation/readMe.md
@@ -1,1 +1,10 @@
+## What did I learn in this chapter?
 
+    - React element
+    - JSX
+    - Always use camelCase while giving attribute to JSX
+    - React component
+    - React Functional component
+    - Component composition (Injecting one component into another)
+    - Injecting javascript code inside a react component using {}
+    - babel (transpilled the jsx code into js code)
