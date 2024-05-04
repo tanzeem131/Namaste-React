4a2fb56d013c97b60d2065f41bdc4e22d8e9e075
tanzeem131 <syedtanzeem131@gmail.com>
2024-05-04 18:07:19 +0530

    CORS plugin issue resolved.

--- Chapter-06-Exploring-the-world/coding/src/components/Body.js
+++ Chapter-06-Exploring-the-world/coding/src/components/Body.js
@@ -1,5 +1,6 @@
 import RestaurantCard from "./RestaurantCard";
 import { useEffect, useState } from "react";
+import { SWIGGY_API } from "../utils/constants";
 
 const Body = () => {
   const [restaurantData, setRestaurantList] = useState([]);
@@ -9,9 +10,7 @@ const Body = () => {
   }, []);
 
   const fetchData = async () => {
-    const data = await fetch(
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=23.7923123251677&lng=86.41775261610746&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
-    );
+    const data = await fetch(SWIGGY_API);
     const json = await data.json();
 
     console.log(json);
