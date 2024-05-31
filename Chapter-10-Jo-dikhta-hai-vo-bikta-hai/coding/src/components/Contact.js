cfd08c4156eaa7ff0ebfd4b87fc9a474e608aa15
tanzeem131 <syedtanzeem131@gmail.com>
2024-05-31 23:50:50 +0530

    final commit

--- Chapter-10-Jo-dikhta-hai-vo-bikta-hai/coding/src/components/Contact.js
+++ Chapter-10-Jo-dikhta-hai-vo-bikta-hai/coding/src/components/Contact.js
@@ -1,6 +1,9 @@
 const Contact = ()=>{
     return (
-        <div className="contact">Contact us</div>
+        <div className="h-[555px] p-4 m-4">
+            <h1 className="text-4xl font-bold">Contact us</h1>
+            <h1 className="text-2xl font-semibold my-4">Write an email 📩 to us at <a className="font-bold">lazeez.khana@gmail.com</a></h1>
+        </div>
     )
 }
 export default Contact;
